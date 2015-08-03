dc351c111058cf12961de96e84c6c1d1ae37e106
unknown <yooasd11@tera2419.bluehole.net>
2015-08-03 17:08:53 +0900

    sadqd

--- Project/Worms/Classes/MoveRequestExecuter.cpp
+++ Project/Worms/Classes/MoveRequestExecuter.cpp
@@ -22,7 +22,7 @@ void MoveRequestExecuter::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, 
 		c_move.SerializeToArray(sendBuf, c_move.ByteSize());
 		ConnectionManager::getInstance()->transmit(c_move.ByteSize(), PACKET_TYPE::PKT_C_MOVE, sendBuf);
 	}
-	if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_A){
+	if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_D){
 		c_move.set_unit_vx(1.0f);
 		c_move.SerializeToArray(sendBuf, c_move.ByteSize());
 		ConnectionManager::getInstance()->transmit(c_move.ByteSize(), PACKET_TYPE::PKT_C_MOVE, sendBuf);
