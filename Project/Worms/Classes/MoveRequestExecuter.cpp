3a061aa57227f4121d842d1cdfa9a9284f1deda4
unknown <yooasd11@tera2419.bluehole.net>
2015-08-05 16:09:22 +0900

    asdadqqqqqq

--- Project/Worms/Classes/MoveRequestExecuter.cpp
+++ Project/Worms/Classes/MoveRequestExecuter.cpp
@@ -47,6 +47,28 @@ void MoveRequestExecuter::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, 
 
 }
 
+//void MoveRequestExecuter::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* pEvent){
+//	JYObject* pOwner = this->getOwner();
+//	if (pOwner == nullptr) return;
+//	cocos2d::CCSprite* pCCOwner = (cocos2d::CCSprite*)pOwner->getCCObject();
+//	if (pCCOwner == nullptr) return;
+//
+//	float fX = pCCOwner->getPosition().x;
+//	float fY = pCCOwner->getPosition().y;
+//
+//	char sendBuf[PKTBODYSIZE];
+//	InGamePacket::C_Stop c_stop;
+//
+//	c_stop.set_uid(this->getOwner()->getUID());
+//	c_stop.set_x(fX);
+//	c_stop.set_y(fY);
+//
+//	c_stop.SerializeToArray(sendBuf, c_stop.ByteSize());
+//
+//	if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_A || keyCode == cocos2d::EventKeyboard::KeyCode::KEY_D)
+//		ConnectionManager::getInstance()->transmit(c_stop.ByteSize(), PACKET_TYPE::PKT_C_STOP, sendBuf);
+//}
+
 void MoveRequestExecuter::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* pEvent){
 	JYObject* pOwner = this->getOwner();
 	if (pOwner == nullptr) return;
@@ -57,14 +79,20 @@ void MoveRequestExecuter::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode,
 	float fY = pCCOwner->getPosition().y;
 
 	char sendBuf[PKTBODYSIZE];
-	InGamePacket::C_Stop c_stop;
+	InGamePacket::C_Move c_move;
 
-	c_stop.set_uid(this->getOwner()->getUID());
-	c_stop.set_x(fX);
-	c_stop.set_y(fY);
+	c_move.set_uid(this->getOwner()->getUID());
 
-	c_stop.SerializeToArray(sendBuf, c_stop.ByteSize());
-
-	if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_A || keyCode == cocos2d::EventKeyboard::KeyCode::KEY_D)
-		ConnectionManager::getInstance()->transmit(c_stop.ByteSize(), PACKET_TYPE::PKT_C_STOP, sendBuf);
+	if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_A){
+		c_move.set_unit_vx(1.0f);
+		c_move.set_unit_vy(0.0f);
+		c_move.SerializeToArray(sendBuf, c_move.ByteSize());
+		ConnectionManager::getInstance()->transmit(c_move.ByteSize(), PACKET_TYPE::PKT_C_MOVE, sendBuf);
+	}
+	if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_D){
+		c_move.set_unit_vx(-1.0f);
+		c_move.set_unit_vy(0.0f);
+		c_move.SerializeToArray(sendBuf, c_move.ByteSize());
+		ConnectionManager::getInstance()->transmit(c_move.ByteSize(), PACKET_TYPE::PKT_C_MOVE, sendBuf);
+	}
 }
